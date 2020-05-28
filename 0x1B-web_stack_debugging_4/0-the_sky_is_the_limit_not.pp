615928ab1db0a51a1549578eccdf4743f8edbb1f
Jose Diaz <jodia.ms@outlook.com>
2020-05-28 12:26:30 -0400

    1 and 2

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+#Change the OS configuration
 exec { 'upperlimit' :
   command =>  'sed -i "s/15/10000/g" /etc/default/nginx;service nginx restart',
   path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
